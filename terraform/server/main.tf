e15e6c4f934dc71929ad479ccf271dabb123e4aa
robertpountney92 <robertpountney92@gmail.com>
2019-07-24 10:46:30 +0100

    added python installation to remote exec terraform - fixed typo

--- terraform/server/main.tf
+++ terraform/server/main.tf
@@ -102,7 +102,7 @@ resource "aws_instance" "web" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update -y",
-      "sudo apt-get intsall python",
+      "sudo apt-get install python",
     ]
   }
   connection {
